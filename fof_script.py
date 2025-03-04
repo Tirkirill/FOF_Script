209934c501746e5f22e6c2ef62011806635be2e4
tirkirill <sherbakirill@mail.ru>
2025-03-04 11:09:26 +0300

    Вторая версия

--- fof_script.py
+++ fof_script.py
@@ -1,22 +1,9 @@
 import random
+from CONSTS import WEAPONS, BOT_NAMES
 
 text:str = "\"BotList\"\n{"
 
-rand_names: str = [
-    "VIP Sex Energy VIP", 
-    "HekI/Ita }I{pET kaLLLLL", 
-    "#~PaP[ER]~Ph[a]Nt0m~#",
-    "FapToBbIu^", 
-    "TaTaPuH", 
-    "Ha_KoJleHu_OJleHu", 
-    "XuJIuGan", 
-    "BeJIuKuu*", 
-    "6u6JIuk", 
-    "4ekHyTbIu*", 
-    "PyLu_Ot_BaByLu"
-    "X.A.K.E.R",
-    "CCCP/KilleR"
-]
+weapons:list[str] = [40, 41, 42]
 
 for i in range(25):
 
@@ -26,8 +13,11 @@ for i in range(25):
     bot_strafe:int         = random.randint(1, 10)
     bot_force_team:int     = random.randint(1, 4)
     bot_aggression:int     = random.randint(1, 10)
-    bot_name:int           = random.choice(rand_names) + "   " + str(random.randint(1, 1000))
-    bot_start_weapon: int  = 7 # Динамит
+    bot_name:int           = random.choice(BOT_NAMES) + "   " + str(random.randint(1, 1000))
+    bot_start_weapon:int   = 26
+    bot_start_weapon_2:int = 6
+    bot_start_weapon_3:int = -1
+    bot_start_weapon_4:int = -1
 
     text += "\n \"preset\" \n  {"
     text += "\n    \"bot_rotation_speed\"\t\t \"{0}\"".format(bot_rotation_speed)
@@ -37,7 +27,12 @@ for i in range(25):
     text += "\n    \"bot_force_team\"\t\t \"{0}\"".format(bot_force_team)
     text += "\n    \"bot_aggression\"\t\t \"{0}\"".format(bot_aggression)
     text += "\n    \"bot_name\"\t\t \"{0}\"".format(bot_name)
-    text += "\n    \"bot_equipment\"\t\t \"{0},{1},{2},{3}\"".format(bot_start_weapon, -1, -1, -1)
+    text += "\n    \"bot_equipment\"\t\t \"{0},{1},{2},{3}\"".format(
+        bot_start_weapon, 
+        bot_start_weapon_2, 
+        bot_start_weapon_3, 
+        bot_start_weapon_4
+    )
     text += "\n  }"
 
 text += "\n}"
